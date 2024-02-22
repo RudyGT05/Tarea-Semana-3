bb0a2c318e8f610675ebec882ba3b4ef786c3672
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-22 09:37:22 -0600

    Primer Ejercicio

--- Tarea Semana 3/Program.cs
+++ Tarea Semana 3/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int prim, segun, tern;
+try
+{
+    Console.WriteLine("Ingrese el primer numero: ");
+    prim = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el segundo numero: ");
+    segun = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese el tercer numero: ");
+    tern = int.Parse(Console.ReadLine());
+    if (prim > segun && prim > tern)
+        Console.WriteLine("El mayor es: " + prim);
+    else if (segun > prim && segun > tern)
+        Console.WriteLine("El mayor es: " + segun);
+    else if (tern > segun && tern > prim)
+        Console.WriteLine("El mayor es: " + tern);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Solo se permiten numeros " + ex.Message);
+}
+
