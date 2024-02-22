42eaf9b4d307e8c5a623138bfd3001b55458a7e2
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-22 10:53:46 -0600

    2.1 Ejercicio

--- Tarea Semana 3/Program.cs
+++ Tarea Semana 3/Program.cs
@@ -1,42 +1,35 @@
-﻿int opcion, baset, alturat, areat, ladocua, areacua, radiocir;
-double areacir;
-
+﻿int opcion;
 try
 {
-    Console.WriteLine("Bienvenido, elija la opcion deseada");
-    Console.WriteLine("1. Circulo");
-    Console.WriteLine("2. Cuadrado");
-    Console.WriteLine("3. Triangulo");
+    Console.WriteLine("Escribe un numero del 1 al 5");
+    
     opcion = int.Parse(Console.ReadLine());
 
-
-
-    if (opcion == 1)
+    switch (opcion)
     {
-
-        Console.WriteLine("Ingrese el radio del circulo");
-        radiocir = int.Parse(Console.ReadLine());
-        areacir = (radiocir * radiocir) +3.1416;
-        Console.WriteLine("El Area del circulo es de: " + areacir);
-
+        case 1:
+            Console.WriteLine("Uno");
+            break;
+        case 2:
+            Console.WriteLine("Dos");
+            break;
+        case 3:
+            Console.WriteLine("Tres");
+            break;
+        case 4:
+            Console.WriteLine("Cuatro");
+            break;
+        case 5:
+            Console.WriteLine("Cinco");
+            break;
+        default:
+            Console.WriteLine("Numero no disponible");
+            break;
     }
-    else if (opcion == 2)
-    {
-        Console.WriteLine("Ingrese el largo del cuadrado");
-        ladocua = int.Parse(Console.ReadLine());
-        areacua = ladocua * ladocua;
-        Console.WriteLine("El Area del cuadrado es de: " + areacua);    
 
-    }
-    else if (opcion == 3)
-    {
-        Console.WriteLine("Ingrese la base del triangulo");
-        baset = int.Parse (Console.ReadLine());
-        Console.WriteLine("Ingrese la altura del triangulo");
-        alturat = int.Parse (Console.ReadLine());
-        areat = (baset * alturat) / 2;
-        Console.WriteLine("El area del triangulo es: " + areat);
-    }
+
+
+   
 }
 catch (Exception ex)
 {
