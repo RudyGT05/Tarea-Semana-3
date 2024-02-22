7d5bdda97d81b81ec3eba0a1b43fbbe1b1c37914
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-22 11:01:53 -0600

    2.2 Ejercicio

--- Tarea Semana 3/Program.cs
+++ Tarea Semana 3/Program.cs
@@ -1,26 +1,32 @@
 ﻿int opcion;
 try
 {
-    Console.WriteLine("Escribe un numero del 1 al 5");
+    Console.WriteLine("Escribe un numero del 1 al 7, para ver a que dia de la semana corresponde");
     
     opcion = int.Parse(Console.ReadLine());
 
     switch (opcion)
     {
         case 1:
-            Console.WriteLine("Uno");
+            Console.WriteLine("Lunes");
             break;
         case 2:
-            Console.WriteLine("Dos");
+            Console.WriteLine("Martes");
             break;
         case 3:
-            Console.WriteLine("Tres");
+            Console.WriteLine("Miercoles");
             break;
         case 4:
-            Console.WriteLine("Cuatro");
+            Console.WriteLine("Jueves");
             break;
         case 5:
-            Console.WriteLine("Cinco");
+            Console.WriteLine("Viernes");
+            break;
+        case 6: 
+            Console.WriteLine("Sabado");
+            break;
+        case 7:
+            Console.WriteLine("Domingo");
             break;
         default:
             Console.WriteLine("Numero no disponible");
