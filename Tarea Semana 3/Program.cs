75402637b1d602a3432df00aa791734c48b098ed
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-22 11:42:29 -0600

    2.5 CON MODIFICACIONES

--- Tarea Semana 3/Program.cs
+++ Tarea Semana 3/Program.cs
@@ -6,33 +6,35 @@ try
 
     
     opcion = int.Parse(Console.ReadLine());
+  
 
-    switch (opcion)
-    {
-        case >=60 and <=69:
-            Console.WriteLine("No aprobatoria");
-            break;
-        case >=70 and <=79:
-            Console.WriteLine("Aprobatoria");
-            break;
-        case >=80 and <=89:
-            Console.WriteLine("Notable");
-            break;
-        case >= 90 and <= 100:
-            Console.WriteLine("Sobresaliente");
-            break;
-       
-        default:
-            Console.WriteLine("La nota no puede ser evaludad");
-            break;
-    }
+        switch (opcion)
+        {
+            case >= 0 and <= 60:
+                Console.WriteLine("No aprobatoria");
+                break;
+            case >= 61 and <= 69:
+                Console.WriteLine("Aprobatoria");
+                break;
+            case >= 70 and <= 79:
+                Console.WriteLine("Satisfactoria");
+                break;
+            case >= 80 and <= 89:
+                Console.WriteLine("Notable");
+                break;
+            case >= 90 and <= 100:
+                Console.WriteLine("Sobresaliente");
+                break;
 
-
+            default:
+                Console.WriteLine("La nota no puede ser evaludad");
+                break;
+        }
 
    
 }
 catch (Exception ex)
 {
-    Console.WriteLine("Solo se pueden escribir numeros "+ ex.Message);
+    Console.WriteLine("ESTE NUMERO NO ES ELEGIBLE PORQUE NO ES ENTERO "+ ex.Message);
 }
 
