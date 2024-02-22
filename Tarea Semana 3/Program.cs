830afbcb5b4c77b52c657f51355e585ffd9c9ff5
RudyGT05 <rgonzaleza11@miumg.edu.gt>
2024-02-22 10:33:46 -0600

    Septimo EJercicio

--- Tarea Semana 3/Program.cs
+++ Tarea Semana 3/Program.cs
@@ -1,18 +1,41 @@
-﻿int num, edad;
+﻿int opcion, baset, alturat, areat, ladocua, areacua, radiocir;
+double areacir;
 
 try
 {
-    Console.WriteLine("Ingrese su edad: ");
-    edad = int.Parse(Console.ReadLine());
-    Console.WriteLine("Ingrese el monto deseado: ");
-    num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Bienvenido, elija la opcion deseada");
+    Console.WriteLine("1. Circulo");
+    Console.WriteLine("2. Cuadrado");
+    Console.WriteLine("3. Triangulo");
+    opcion = int.Parse(Console.ReadLine());
 
 
-    if (edad >= 60 || num <= 5000) { 
 
-    Console.WriteLine("Su prestamo ha sido aprobado");
-}
-    else {Console.WriteLine("Lamentablemente su prestamo ha sido rechazado");
+    if (opcion == 1)
+    {
+
+        Console.WriteLine("Ingrese el radio del circulo");
+        radiocir = int.Parse(Console.ReadLine());
+        areacir = (radiocir * radiocir) +3.1416;
+        Console.WriteLine("El Area del circulo es de: " + areacir);
+
+    }
+    else if (opcion == 2)
+    {
+        Console.WriteLine("Ingrese el largo del cuadrado");
+        ladocua = int.Parse(Console.ReadLine());
+        areacua = ladocua * ladocua;
+        Console.WriteLine("El Area del cuadrado es de: " + areacua);    
+
+    }
+    else if (opcion == 3)
+    {
+        Console.WriteLine("Ingrese la base del triangulo");
+        baset = int.Parse (Console.ReadLine());
+        Console.WriteLine("Ingrese la altura del triangulo");
+        alturat = int.Parse (Console.ReadLine());
+        areat = (baset * alturat) / 2;
+        Console.WriteLine("El area del triangulo es: " + areat);
     }
 }
 catch (Exception ex)
